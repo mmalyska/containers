91c49954c2d1c3f30bb1ca77f5718e477a1462c0
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2023-03-06 21:44:49 +0100

    fix logs in script

--- .github/scripts/index.mjs
+++ .github/scripts/index.mjs
@@ -15,11 +15,11 @@ export const changes = async (glob, context, github, core, all = false) => {
       let change = {"app": app, "channel": channel.name, "version": upstreamVersion};
       if (all) {
         changes.push(change);
-        console.log(`Pushed changes ${change}`);
+        console.log(`Pushed changes ${JSON.parse(change)}`);
       }
       else if (publishedVersion != upstreamVersion.stdout) {
         changes.push(change);
-        console.log(`Pushed changes ${change}`);
+        console.log(`Pushed changes ${JSON.parse(change)}`);
       }
     }
   }
@@ -42,7 +42,7 @@ export const appChanges = async (core, apps, overrideChannels) => {
     for (const channel of channels) {
       let upstreamVersion = await upstream(app, channel.name, channel.stable);
       let change = {"app": app, "channel": channel.name, "version": upstreamVersion};
-      console.log(`Pushed changes ${change}`);
+      console.log(`Pushed changes ${JSON.parse(change)}`);
       changes.push(change);
     }
   }
@@ -70,6 +70,7 @@ const published = async (context, github, core, app, channel, stable) => {
       package_name: app,
       username: context.repo.owner,
     });
+    console.log(`response ${res}`);
     const rollingContainer = res.find(e => e.metadata.container.tags.includes("rolling"));
     return rollingContainer.metadata.container.tags.find(e => e != "rolling");
   } catch (error) {
