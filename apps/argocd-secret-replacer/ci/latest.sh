8b2cc82f3c5bd36fff29676682df7caa213a9963
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2023-03-13 12:56:47 +0100

    fix latest file

--- apps/argocd-secret-replacer/ci/latest.sh
+++ apps/argocd-secret-replacer/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/mmalyska/aargocd-secret-replacer/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
+version="$(curl -sX GET "https://api.github.com/repos/mmalyska/argocd-secret-replacer/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
