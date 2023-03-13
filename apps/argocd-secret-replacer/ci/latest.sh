cd8f0511c7cc5fec51c0e5d9254c2fca57c9dcb2
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2023-03-13 13:53:24 +0100

    change latest script

--- apps/argocd-secret-replacer/ci/latest.sh
+++ apps/argocd-secret-replacer/ci/latest.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 version="$(curl -sX GET "https://api.github.com/repos/mmalyska/argocd-secret-replacer/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
 version="${version#*v}"
-version="${version#*release-}"
 printf "%s" "${version}"
