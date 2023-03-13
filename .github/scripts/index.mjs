911cd3dbc8f33bac92ec805c28c1635bc45640fe
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2023-03-13 10:31:40 +0100

    try catch published

--- .github/scripts/index.mjs
+++ .github/scripts/index.mjs
@@ -10,7 +10,7 @@ export const changes = async (glob, context, github, core, all = false) => {
     let { app, channels } = JSON.parse(channelsFile);
 
     for (const channel of channels) {
-      let publishedVersion = await published(context, github, core, app, channel.name, channel.stable);
+      let {found: publishedFound, version: publishedVersion} = await published(context, github, core, app, channel.name, channel.stable) || {};
       let upstreamVersion = await upstream(app, channel.name, channel.stable);
       let change = { "app": app, "channel": channel.name, "version": upstreamVersion };
       if (upstreamVersion == null || publishedVersion == null) {
@@ -20,7 +20,7 @@ export const changes = async (glob, context, github, core, all = false) => {
         changes.push(change);
         console.log(`Pushed changes "app": ${app}, "channel": ${channel.name}, "version": ${upstreamVersion}, "published": ${publishedVersion}`);
       }
-      else if (!publishedVersion.found || publishedVersion.version != upstreamVersion) {
+      else if (!publishedFound || publishedVersion != upstreamVersion) {
         changes.push(change);
         console.log(`Pushed changes "app": ${app}, "channel": ${channel.name}, "version": ${upstreamVersion}, "published": ${publishedVersion}`);
       }
