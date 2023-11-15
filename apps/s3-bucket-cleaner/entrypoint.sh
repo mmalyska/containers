8a13a3ce0288ff6d4541e9e3d914ba660300c4fb
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2023-11-15 16:11:21 +0100

    Update entrypoint.sh

--- apps/s3-bucket-cleaner/entrypoint.sh
+++ apps/s3-bucket-cleaner/entrypoint.sh
@@ -4,6 +4,6 @@ args=()
 [ ! -z "$DRYRUN" ] && args+=( '--dryrun' )
 
 aws s3api list-objects-v2 --bucket "$BUCKET" --prefix "$S3_PREFIX" --query \
-     "Contents[?LastModified<=\`$(date -d '30 days ago' '+%Y-%m-%d')\`][].{object: Key}" \
+     "Contents[?LastModified<=\`$(date -d "$OLDER_THAN" '+%Y-%m-%d')\`][].{object: Key}" \
   | jq -r '.[].object' \
   | xargs -I {} aws s3 rm s3://$BUCKET/{} $args
