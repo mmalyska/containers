3450a7d3a1151b9cd2760c3055827bbd0243f5e4
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2023-11-15 13:27:48 +0100

    add new container - s3 cleaner (#101)

---
+++ apps/s3-bucket-cleaner/entrypoint.sh
@@ -0,0 +1,7 @@
+args=()
+[ ! -z "$DRYRUN" ] && args+=( '--dryrun' )
+
+aws s3api list-objects-v2 --bucket "$BUCKET" --prefix "$S3_PREFIX" --query \
+     "Contents[?LastModified<=\`$(date -d '30 days ago' '+%Y-%m-%d')\`][].{object: Key}" \
+  | jq -r '.[].object' \
+  | xargs -I {} aws s3 rm s3://$BUCKET/{} $args
