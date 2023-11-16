96473f4e59834ed1595abd5f48b67e2b4fdbed0a
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2023-11-16 09:53:27 +0000

    change to new api remove object

--- apps/s3-bucket-cleaner/entrypoint.sh
+++ apps/s3-bucket-cleaner/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-args=()
-[ ! -z "$DRYRUN" ] && args+=( '--dryrun' )
-
 aws s3api list-objects-v2 --bucket "$BUCKET" --prefix "$S3_PREFIX" --query \
      "Contents[?LastModified<=\`$(date -d "$OLDER_THAN" '+%Y-%m-%d')\`][].{object: Key}" \
   | jq -r '.[].object' \
-  | xargs -I {} aws s3 rm s3://$BUCKET/{} $args
+  | xargs -I {} aws s3api delete-object --bucket "$BUCKET" --key {}
