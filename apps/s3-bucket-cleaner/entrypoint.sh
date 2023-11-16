25e5a8830912812195fea6f3265e07dc52b98fec
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2023-11-16 11:20:44 +0000

    fix url encoded key names

--- apps/s3-bucket-cleaner/entrypoint.sh
+++ apps/s3-bucket-cleaner/entrypoint.sh
@@ -2,5 +2,5 @@
 
 aws s3api list-objects-v2 --bucket "$BUCKET" --prefix "$S3_PREFIX" --query \
      "Contents[?LastModified<=\`$(date -d "$OLDER_THAN" '+%Y-%m-%d')\`][].{object: Key}" \
-  | jq -r '.[].object' \
-  | xargs -I {} aws s3api delete-object --bucket "$BUCKET" --key {}
+  | jq -r 'include "urldecode" {search: "/var/lib/jq"} ; .[].object | urldecode' \
+  | xargs -I {} aws s3api delete-object --bucket "$BUCKET" --key "{}"
