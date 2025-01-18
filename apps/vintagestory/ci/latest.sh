4f46b83dade5a87ad96245c3987640f3c3a73361
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2025-01-18 17:06:19 +0000

    remove v from version number

--- apps/vintagestory/ci/latest.sh
+++ apps/vintagestory/ci/latest.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 version="$(curl -sX GET "https://mods.vintagestory.at/api/gameversions" | jq --raw-output '.gameversions[0].name' 2>/dev/null)"
+version="${version#*v}"
 printf "%s" "${version}"
