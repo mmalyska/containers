1b32a1b41b4c08aa62ef6f76ef24d8d10749828a
Michał Małyska <999598+mmalyska@users.noreply.github.com>
2026-05-10 19:00:04 +0000

    fix(azerothcore): use jq instead of python in latest.sh

--- apps/azerothcore-worldserver/ci/latest.sh
+++ apps/azerothcore-worldserver/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 version="$(curl -sX GET "https://api.github.com/repos/azerothcore/azerothcore-wotlk/commits?per_page=1&sha=master" 2>/dev/null \
-  | python3 -c "import sys,json; print(json.load(sys.stdin)[0]['commit']['committer']['date'][:10])")"
+  | jq -r '.[0].commit.committer.date[:10]')"
 printf "%s" "${version}"
